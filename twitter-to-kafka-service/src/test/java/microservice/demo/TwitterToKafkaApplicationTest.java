6a8fe22c972f42db545dc151bd67368bdbe768dc
Charles Hayes <cfhayes16@gmail.com>
2024-10-17 07:15:04 -0600

    Created the starter code for a new microservice that will load twitter feeds into kafka

---
+++ twitter-to-kafka-service/src/test/java/microservice/demo/TwitterToKafkaApplicationTest.java
@@ -0,0 +1,14 @@
+package microservice.demo;
+
+import microservices.demo.TwitterToKafkaApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TwitterToKafkaApplication.class)
+public class TwitterToKafkaApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
